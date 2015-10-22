809b958afc288dae4b3e59f6d05f83577d3e3425
Kushal K Dey <kshldey@gmail.com>
2015-10-22 02:12:40 -0500

    sped up the code

--- project/R/cell_order/example_sim.R
+++ project/R/cell_order/example_sim.R
@@ -6,28 +6,21 @@ num_cells <- 400;
 amp_genes <- rep(10, G);
 phi_genes <- runif(G, 0, 2*pi)
 sigma_genes <- rchisq(G, 4);
-cell_times <- seq(0,2*pi, 2*pi/(num_cells-1));
+cell_times_sim <- sample(seq(0,2*pi, 2*pi/(num_cells-1)), num_cells, replace=FALSE);
 
-cycle_data <- sim_sinusoidal_cycle(G, amp_genes, phi_genes, sigma_genes, cell_times);
+cycle_data <- sim_sinusoidal_cycle(G, amp_genes, phi_genes, sigma_genes, cell_times_sim);
 
 celltime_levels <- 100;
 
-molecules_single_cell_cycle <- read.table("../data/molecules_ipsc_single_cell_cycle.txt");
-cycle_data <- t(molecules_single_cell_cycle);
-cycle_data <- cycle_data[, which(colSums(cycle_data)!=0)]
+out <- cell_reordering_phase(cycle_data, celltime_levels = 100, num_iter=100)
 
-## compare the SNR for Yoav's cell cycle data
+  
+plot(amp_genes, out$amp, col="red",xlab="true amplitudes", ylab="est amplitudes", main="amplitudes est, comparison")
+plot(sigma_genes, out$sigma, col="red",xlab="true sigma", ylab="est sigma", main="sigma(variation) est, comparison")
+plot(phi_genes, out$phi, col="red",xlab="true phi", ylab="est phi", main="phase est, comparison");
 
-load_data <- get(load(file="../rdas/cell_order_ipsc.rda"));
-amp_genes <- load_data$amp;
-sd_genes <- load_data$sigma;
-
-ESS <- amp_genes^2; RSS <- sd_genes^2
-
-SNR <- ESS/RSS;
-
-plot(SNR, xlab="genes", ylab="SNR", main="Plot of SNR across all genes")
-
-SNR_indices <- which(SNR > 10);
-
-cycle_data <- cycle_data[,SNR_indices];
+library(plotrix)
+radial.plot(lengths=1:length(out$cell_times),radial.pos=out$cell_times[order(cell_times_sim)], 
+            line.col=colorRampPalette(brewer.pal(9,"Blues"))(length(out$cell_times)), lwd=2)
+radial.plot(lengths=1:length(cell_times_sim),radial.pos=sort(cell_times_sim), 
+            line.col=colorRampPalette(brewer.pal(9,"Blues"))(length(cell_times_sim)), lwd=2)
