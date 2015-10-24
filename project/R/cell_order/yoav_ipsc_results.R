177b02cd06404421aa882423746d6a650f22546b
Kushal K Dey <kshldey@gmail.com>
2015-10-23 19:37:20 -0500

    added .gitignore

--- project/R/cell_order/yoav_ipsc_results.R
+++ project/R/cell_order/yoav_ipsc_results.R
@@ -32,6 +32,15 @@ load_data <- get(load(file="../rdas/cell_order_ipsc.rda"));
 
 cell_times <- load_data$cell_times;
 
+vioplot(cell_times[which(cell_phase_vector=="G1.S")],
+        cell_times[which(cell_phase_vector=="S")],
+        cell_times[which(cell_phase_vector=="G2.M")],
+        cell_times[which(cell_phase_vector=="M")],
+        cell_times[which(cell_phase_vector=="M.G1")],
+        names=c("G1.S","S","G2.M","M","M.G1"),
+        col="red")
+
+
 rank_celltimes <- rank(cell_times);
 
 plot(rank_celltimes, rank_pca, xlab="Rank (ordering)",
@@ -39,6 +48,12 @@ plot(rank_celltimes, rank_pca, xlab="Rank (ordering)",
 amp_genes <- load_data$amp;
 sd_genes <- load_data$sigma;
 phi_genes <- load_data$phi;
+
+cell_order_full <- cell_reordering_full(cycle_data, celltime_levels=100, cell_times, amp_genes, phi_genes, sd_genes)
+library(plotrix)
+radial.plot(lengths=1:length(cell_order_full),radial.pos=as.vector(cell_order_full), 
+            line.col=factor(cell_phase_vector), lwd=2)
+
 
 ESS <- amp_genes^2; RSS <- sd_genes^2
 
@@ -55,7 +70,34 @@ cell_cycle_genes_list <- lapply(1:5,function(x){
   temp[temp!=""]
 })
 
+order1 <- match(cell_cycle_genes_list[[1]], colnames(cycle_data)); order1 <- order1[which(!is.na(order1))];
+order2 <- match(cell_cycle_genes_list[[2]], colnames(cycle_data)); order2 <- order2[which(!is.na(order2))]
+order3 <- match(cell_cycle_genes_list[[3]], colnames(cycle_data)); order3 <- order3[which(!is.na(order3))]
+order4 <- match(cell_cycle_genes_list[[4]], colnames(cycle_data)); order4 <- order4[which(!is.na(order4))]
+order5 <- match(cell_cycle_genes_list[[5]], colnames(cycle_data)); order5 <- order5[which(!is.na(order5))]
 
+order <- c(order1, order2, order3, order4, order5);
+
+phase_genes <- c(rep("G1.S",length(order1)), rep("S", length(order2)), rep("G2.M", length(order3)), rep("M", length(order4)),
+                 rep("M.G1", length(order5)));
+
+order_unique <- unique(order);
+
+phase_genes <- phase_genes[match(order_unique, order)];
+
+phi_genes_yoav <- c(rep((11/(2*24))*2*pi, length(order1)), rep((11/24 + (19-11)/(2*24))*2*pi, length(order2)),
+               rep((19/24 + ((23-19)/(2*24)))*2*pi, length(order3)), rep((23/24)*2*pi, length(order4)),
+               rep((23/24)*2*pi, length(order5)));
+
+phi_genes_yoav <- phi_genes_yoav[match(order_unique, order)];
+
+out <- cell_reordering_phase(cycle_data, celltime_levels = 100, num_iter=100, 
+                             save_path="../rdas/cell_order_ipsc_phase_fixed.rda",
+                             fix.phase = TRUE, phi_fixed = phi_genes_yoav)
+
+
+
+  
 phi_vec <- array(0, 0);
 factor_level <- array(0,0);
 cell_phase <- colnames(cell_cycle_genes)
@@ -76,6 +118,11 @@ plot(ordered(factor_vec, c("G1.S", "S", "G2.M", "M", "M.G1")), phi_vec1,
      xlab="factor_levels", ylab="phase (radians)", 
      main="Phase plot across cell phases", col="red")
 
+plot(ordered(c("G1.S", "S", "G2.M", "M", "M.G1"),c("G1.S", "S", "G2.M", "M", "M.G1")), phi_vec1,
+     xlab="factor_levels", ylab="phase (radians)", 
+     main="Phase plot across cell phases", col="red")
+
+
 amp_vec <- array(0, 0);
 factor_level <- array(0,0);
 
@@ -91,7 +138,7 @@ amp_vec1 <- unique(amp_vec);
 factor_vec <- factor_level[match(amp_vec1,amp_vec)]
 
 par(mar=c(4,1,1,1))
-plot(ordered(factor_vec, c("G1.S", "S", "G2.M", "M", "M.G1")), amp_vec1,
+plot(ordered(factor_vec, c("G1.S", "S", "G2.M", "M", "M.G1")), amp_vec1/1000,
      xlab="factor_levels", ylab="amplitudes", 
      main="Amplitude plot across cell phases", col="red")
 
